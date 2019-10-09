99c1212b3ad1cd2c7d4b309209765f98a984bfdd
Kames <info@kamescg.com>
2019-10-08 20:58:27 -0400

    Updates

---
+++ precache-manifest.985af1ac5157e9f570168137330c5f97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2599091eb9f2485bf60272eada049742",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d244232197deb1673368",
+    "url": "/static/css/main.80e53177.chunk.css"
+  },
+  {
+    "revision": "7f290f83ff7d215fd9c9",
+    "url": "/static/js/2.62a75b1c.chunk.js"
+  },
+  {
+    "revision": "d244232197deb1673368",
+    "url": "/static/js/main.653440ef.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
